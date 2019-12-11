72555b117e904b5943a60516ba794c3569ff1c65
Delta Echo <echoair@t-online.de>
2019-12-11 17:34:44 +0100

    Add Munich, add Airforce bases

---
+++ get_osm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cp -r ../osm_data/germany/work/* work-osm/
+for i in output/Terrain/*; do
+	for k in $i/*; do
+		mkdir work-osm/Default
+		mkdir work-osm/Default/`echo $i | cut -d / -f 3`
+		mkdir work-osm/Default/`echo $i | cut -d / -f 3`/`echo $k | cut -d / -f 4`
+		cp -r ../osm_data/work/Default/`echo $i | cut -d / -f 3`/`echo $k | cut -d / -f 4`/*  work-osm/Default/`echo $i | cut -d / -f 3`/`echo $k | cut -d / -f 4`/
+	done;
+done
--- groundnets.sh
+++ groundnets.sh
@@ -1,3 +1,8 @@
 cd groundnet
-for i in ../data/airports/region$1/*.dat; do cp $i .; rm apt.dat; mv *.dat apt.dat; python aptdat2sqlite.py; python sqlite2xml.py; done
+if [ $1 = "big" ]
+then
+	for i in ../data/airports/big_airports/*.dat; do cp $i .; rm apt.dat; mv *.dat apt.dat; python aptdat2sqlite.py; python sqlite2xml.py; done
+else
+	for i in ../data/airports/region$1/*.dat; do cp $i .; rm apt.dat; mv *.dat apt.dat; python aptdat2sqlite.py; python sqlite2xml.py; done
+fi
 cp -r groundnet/Airports output/
